370a0076823f9ac1fb486d08f89f00f1bcf03b2e
Darshan T <darshandarsh.t1999@gmail.com>
2026-08-11 14:59:39 +0530

    Update S3 bucket and DynamoDB table in backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,16 +1,18 @@
 terraform {
   required_version = "~> 1.9.3"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.49.0"
     }
   }
+
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "darshan-eks-terraform-state-2026"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "darshan-terraform-lock"
     encrypt        = true
   }
 }
